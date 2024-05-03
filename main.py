145adabb5aa60c52fab11f495d563dc8fffdf1c8
Manpreet Singh <msp99000@gmail.com>
2024-05-03 20:19:10 +0530

    feat: modifies header, logo, description and button alignment

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from streamlit_extras.switch_page_button import switch_page
 streamlit_style()
 
 # App Logo
-st.image("images/transparent_logo.png")
+a, b, c = st.columns([1, 3, 1])
+
+with b:
+    st.image("images/transparent_logo.png")
 
 # Add header
-st.title("Zestimate - Forecasting Property Growth")
+st.write("")
+st.header("Zestimate - Forecasting Property Growth")
+st.write("")
 
 # Add description
 description = """
@@ -16,13 +21,6 @@ Welcome to Zestimate – your ultimate destination for accurate housing predicti
 
 At Zestimate, we understand the importance of making informed decisions in the dynamic real estate market. Whether you are a homeowner, real estate investor, or industry professional, our platform provides invaluable insights to help you navigate the ever-changing landscape of property values.
 
-Our approach leverages state-of-the-art time series analysis and predictive modeling to deliver precise forecasts of ZHVI and ZHVF based on a rich set of features, including historical property data, economic indicators, and local market trends. By analyzing vast amounts of data, we identify patterns and trends that enable us to anticipate future changes in home values with unparalleled accuracy.
-
-With Zestimate, you can gain a competitive edge in the real estate market. Our intuitive platform offers easy access to detailed forecasts, customizable reports, and interactive visualizations, empowering you to make strategic decisions confidently. Whether you're buying, selling, or investing in real estate, Zestimate equips you with the insights you need to succeed.
-
-Join thousands of users who trust Zestimate for their real estate forecasting needs. Start exploring our platform today and unlock the power of predictive analytics to shape your future in real estate.
-
-Unlock Tomorrow's Home Values with Zestimate – Where Insight meets Innovation.
 """
 
 st.write(description)

e9462c445bc89e84370c825a5e31ac1ef4d4ebc4
Manpreet Singh <msp99000@gmail.com>
2024-05-27 15:22:57 +0530

    feat: adds modified project files

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from streamlit_extras.switch_page_button import switch_page
 streamlit_style()
 
 # App Logo
+st.write("")
 a, b, c = st.columns([1, 3, 1])
 
 with b:
@@ -12,19 +13,22 @@ with b:
 
 # Add header
 st.write("")
-st.header("Zestimate - Forecasting Property Growth")
+st.markdown(
+    "<h1 style='text-align: center; color: #47c099;'>Welcome to Zestimate !</h1>",
+    unsafe_allow_html=True,
+)
 st.write("")
 
 # Add description
 description = """
-Welcome to Zestimate – your ultimate destination for accurate housing predictions. Zestimate is a cutting-edge project designed to forecast the Zillow Home Value Index (ZHVI) and Zillow Home Value Forecast (ZHVF) using advanced machine learning techniques on comprehensive housing data.
+Zestimate is your ultimate destination for accurate housing predictions. Zestimate is a cutting-edge project designed to forecast the Zillow Home Value Index (ZHVI) and Zillow Home Value Forecast (ZHVF) using advanced machine learning techniques on comprehensive housing data.
 
 At Zestimate, we understand the importance of making informed decisions in the dynamic real estate market. Whether you are a homeowner, real estate investor, or industry professional, our platform provides invaluable insights to help you navigate the ever-changing landscape of property values.
 
 """
 
 st.write(description)
-st.header(" ")
+st.write("")
 
 
 def main():

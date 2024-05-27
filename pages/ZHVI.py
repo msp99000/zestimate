ca17d7ae0d292bed7a24a5902c41a9ac07644e9b
Manpreet Singh <msp99000@gmail.com>
2024-05-27 18:58:53 +0530

    chore: adds ZHVI predictor

--- pages/ZHVI.py
+++ pages/ZHVI.py
@@ -3,6 +3,10 @@ from styles import streamlit_style
 from streamlit_extras.switch_page_button import switch_page
 from streamlit_option_menu import option_menu
 from sidebar import sidebar_content
+from utils import data_loader, model_loader, pipeline_loader
+import pandas as pd
+from xgboost import XGBRegressor
+import pickle
 
 streamlit_style()
 
@@ -12,7 +16,7 @@ def main():
 
     # Horizontal Menu
     selected = option_menu(None, ["Home", "Statistics", "ZHVI", 'ZORI'], 
-                            icons=['house', 'cloud-upload', "list-task", 'gear'], 
+                            icons=['house', 'graph-up-arrow', "house-up", 'house-up-fill'], 
                             menu_icon="cast", default_index=2, orientation="horizontal")
     
     if selected == 'Statistics':
@@ -24,6 +28,35 @@ def main():
     if selected == 'ZORI':
         switch_page('ZORI')
 
+    df, _ = data_loader()
+    lgbm, xgb = model_loader()
+    pipeline = pipeline_loader()
+
+    # Streamlit app
+    def predict_zhvi(state, type, date):
+        region = 'N/A'
+        date_obj = pd.to_datetime(date)
+        year = date_obj.year
+        month = date_obj.month
+        week = date_obj.isocalendar().week
+        day = date_obj.day
+        encoded_data = pipeline.transform([[region, state, type, year, month, week, day]])
+        
+        model = xgb
+        
+        prediction = model.predict(encoded_data)[0]
+        return prediction
+
+    st.write("")
+    st.title('Zillow Housing ZHVI Prediction')
+
+    state = st.selectbox('State', df['State'].unique())
+    type = st.selectbox('Type', df['Type'].unique())
+    date = st.date_input('Date')
+
+    if st.button('Predict'):
+        predicted_zhvi = predict_zhvi(state, type, date)
+        st.write(f'Predicted ZHVI: ${predicted_zhvi:.2f}')
 
 if __name__ == "__main__":
     main()

6fa3102a6391c24da137708d386ecd17ce205d0f
Manpreet Singh <msp99000@gmail.com>
2024-05-27 15:52:16 +0530

    feat: adds navbar for seamless navigation

--- pages/ZHVI.py
+++ pages/ZHVI.py
@@ -1,3 +1,26 @@
 import streamlit as st
+from styles import streamlit_style
+from streamlit_extras.switch_page_button import switch_page
+from streamlit_option_menu import option_menu
 
-st.title("ZHVI")
+streamlit_style()
+
+def main():
+
+    # Horizontal Menu
+    selected = option_menu(None, ["Home", "Statistics", "ZHVI", 'ZORI'], 
+                            icons=['house', 'cloud-upload', "list-task", 'gear'], 
+                            menu_icon="cast", default_index=2, orientation="horizontal")
+    
+    if selected == 'Statistics':
+        switch_page('Statistics')
+    
+    if selected == 'Home':
+        switch_page('main')
+    
+    if selected == 'ZORI':
+        switch_page('ZORI')
+
+
+if __name__ == "__main__":
+    main()

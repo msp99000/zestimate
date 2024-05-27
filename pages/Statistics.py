5c3457de9c768dadd072d96f92f8ad5d0da2b617
Manpreet Singh <msp99000@gmail.com>
2024-05-27 15:51:39 +0530

    feat: adds navbar for seamless navigation

--- pages/Statistics.py
+++ pages/Statistics.py
@@ -1,3 +1,26 @@
 import streamlit as st
+from styles import streamlit_style
+from streamlit_extras.switch_page_button import switch_page
+from streamlit_option_menu import option_menu
 
-st.title("Statistics")
+streamlit_style()
+
+def main():
+
+    # Horizontal Menu
+    selected = option_menu(None, ["Home", "Statistics", "ZHVI", 'ZORI'], 
+                            icons=['house', 'cloud-upload', "list-task", 'gear'], 
+                            menu_icon="cast", default_index=1, orientation="horizontal")
+    
+    if selected == 'Home':
+        switch_page('main')
+    
+    if selected == 'ZHVI':
+        switch_page('ZHVI')
+    
+    if selected == 'ZORI':
+        switch_page('ZORI')
+
+
+if __name__ == "__main__":
+    main()

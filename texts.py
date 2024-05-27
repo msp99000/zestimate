9dc8972cc7a11fc6254ea734f80bdd9c4856cb57
Manpreet Singh <msp99000@gmail.com>
2024-05-27 16:02:23 +0530

    feat: single file to contain textual information

---
+++ texts.py
@@ -0,0 +1,8 @@
+
+# App Description
+description = """
+Zestimate is your ultimate destination for accurate housing predictions. Zestimate is a cutting-edge project designed to forecast the Zillow Home Value Index (ZHVI) and Zillow Home Value Forecast (ZHVF) using advanced machine learning techniques on comprehensive housing data.
+
+At Zestimate, we understand the importance of making informed decisions in the dynamic real estate market. Whether you are a homeowner, real estate investor, or industry professional, our platform provides invaluable insights to help you navigate the ever-changing landscape of property values.
+
+"""

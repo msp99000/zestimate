2795eadd740a36e2768442f52a9792279adbda78
Manpreet Singh <msp99000@gmail.com>
2024-05-27 15:51:20 +0530

    feat: adds navbar for seamless navigation

--- main.py
+++ main.py
@@ -1,37 +1,51 @@
 import streamlit as st
 from styles import streamlit_style
 from streamlit_extras.switch_page_button import switch_page
+from streamlit_option_menu import option_menu
 
 streamlit_style()
 
-# App Logo
-st.write("")
-a, b, c = st.columns([1, 3, 1])
-
-with b:
-    st.image("images/transparent_logo.png")
-
-# Add header
-st.write("")
-st.markdown(
-    "<h1 style='text-align: center; color: #47c099;'>Welcome to Zestimate !</h1>",
-    unsafe_allow_html=True,
-)
-st.write("")
-
-# Add description
-description = """
-Zestimate is your ultimate destination for accurate housing predictions. Zestimate is a cutting-edge project designed to forecast the Zillow Home Value Index (ZHVI) and Zillow Home Value Forecast (ZHVF) using advanced machine learning techniques on comprehensive housing data.
-
-At Zestimate, we understand the importance of making informed decisions in the dynamic real estate market. Whether you are a homeowner, real estate investor, or industry professional, our platform provides invaluable insights to help you navigate the ever-changing landscape of property values.
-
-"""
-
-st.write(description)
-st.write("")
-
-
 def main():
+
+    # Horizontal Menu
+    selected = option_menu(None, ["Home", "Statistics", "ZHVI", 'ZORI'], 
+                            icons=['house', 'cloud-upload', "list-task", 'gear'], 
+                            menu_icon="cast", default_index=0, orientation="horizontal")
+    
+    if selected == 'Statistics':
+        switch_page('Statistics')
+    
+    if selected == 'ZHVI':
+        switch_page('ZHVI')
+    
+    if selected == 'ZORI':
+        switch_page('ZORI')
+
+    # App Logo
+    st.write("")
+    a, b, c = st.columns([1, 3, 1])
+
+    with b:
+        st.image("images/transparent_logo.png")
+
+    # Add header
+    st.write("")
+    st.markdown(
+        "<h1 style='text-align: center; color: #47c099;'>Welcome to Zestimate !</h1>",
+        unsafe_allow_html=True,
+    )
+    st.write("")
+
+    # Add description
+    description = """
+    Zestimate is your ultimate destination for accurate housing predictions. Zestimate is a cutting-edge project designed to forecast the Zillow Home Value Index (ZHVI) and Zillow Home Value Forecast (ZHVF) using advanced machine learning techniques on comprehensive housing data.
+
+    At Zestimate, we understand the importance of making informed decisions in the dynamic real estate market. Whether you are a homeowner, real estate investor, or industry professional, our platform provides invaluable insights to help you navigate the ever-changing landscape of property values.
+
+    """
+
+    st.write(description)
+    st.write("")
 
     zhvi, stats, zori = st.columns(3)
 

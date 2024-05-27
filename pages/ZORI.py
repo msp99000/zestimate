05826abc5a24423897c44eed7b316a43e2df2320
Manpreet Singh <msp99000@gmail.com>
2024-05-27 15:52:24 +0530

    feat: adds navbar for seamless navigation

--- pages/ZORI.py
+++ pages/ZORI.py
@@ -1,3 +1,26 @@
 import streamlit as st
+from styles import streamlit_style
+from streamlit_extras.switch_page_button import switch_page
+from streamlit_option_menu import option_menu
 
-st.title("ZORI")
+streamlit_style()
+
+def main():
+
+    # Horizontal Menu
+    selected = option_menu(None, ["Home", "Statistics", "ZHVI", 'ZORI'], 
+                            icons=['house', 'cloud-upload', "list-task", 'gear'], 
+                            menu_icon="cast", default_index=3, orientation="horizontal")
+    
+    if selected == 'Statistics':
+        switch_page('Statistics')
+    
+    if selected == 'ZHVI':
+        switch_page('ZHVI')
+    
+    if selected == 'Home':
+        switch_page('main')
+
+
+if __name__ == "__main__":
+    main()

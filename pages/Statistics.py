b8e2133557824c5a4de4dcab38121d508e41b375
Manpreet Singh <msp99000@gmail.com>
2024-06-01 15:39:05 +0530

    fix(sidebar): sidebar wide mode turned on

--- pages/Statistics.py
+++ pages/Statistics.py
@@ -7,9 +7,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plots import plot_zhvi, plot_zori
 
-# Set page configuration as the first Streamlit command
-st.set_page_config(page_title="My Streamlit App", layout="wide")
-
 # Apply custom style
 streamlit_style()
 
